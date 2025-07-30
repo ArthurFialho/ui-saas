7145fad10ca9f11a6b174f078e08ab3f949f9943
ARTHUR_FERNANDES_FIALHO_E_SILVA <12301795@ALFAB03.BARROCA>
2025-07-30 11:00:07 -0300

    feat: add right effect animation in Hero component

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,43 +5,101 @@ import designExampleImage from "@/assets/images/design-example-1.png";
 import designExample2Image from "@/assets/images/design-example-2.png";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
-import {motion, useAnimate} from "framer-motion"
+import { motion, useAnimate } from "framer-motion";
 import { useEffect } from "react";
+import cursorYouImage from "@/assets/images/cursor-you.svg";
 
 export default function Hero() {
     const [leftDesignScope, leftDesignAnimate] = useAnimate();
     const [leftPointerScope, leftPointerAnimate] = useAnimate();
-    const [rightDesignScope, /*rightDesignAnimate*/] = useAnimate();
-    const [rightPointerScope, /*rightPointerAnimate*/] = useAnimate();
+    const [rightDesignScope, rightDesignAnimate] = useAnimate();
+    const [rightPointerScope, rightPointerAnimate] = useAnimate();
 
     useEffect(() => {
         leftDesignAnimate([
             [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
-            [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }]
+            [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
         ]);
 
         leftPointerAnimate([
-            [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 } ],
+            [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
             [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
-            [leftPointerScope.current, { x: 0, y: [0, 16, 0]}, { duration: 0.5, ease: "easeInOut" }]
+            [
+                leftPointerScope.current,
+                { x: 0, y: [0, 16, 0] },
+                { duration: 0.5, ease: "easeInOut" },
+            ],
+        ]);
+
+        rightDesignAnimate([
+            [
+                rightDesignScope.current,
+                { opacity: 1 },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightDesignScope.current, { x: 0, y: 0 }, { duration: 0.5 }],
+        ]);
+
+        rightPointerAnimate([
+            [
+                rightPointerScope.current,
+                { opacity: 1 },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightPointerScope.current, { x: 175, y: 0 }, { duration: 0.5 }],
+            [
+                rightPointerScope.current,
+                { x: 0, y: [0, 20, 0] },
+                { duration: 0.5 },
+            ],
         ]);
     }, []);
 
     return (
-
-        <section className="py-24 overflow-x-clip">
+        <section
+            className="py-24 overflow-x-clip"
+            style={{
+                cursor: `url(${cursorYouImage.src}), auto`,
+            }}
+        >
             <div className="container relative">
-                <motion.div ref={leftDesignScope} initial={{ opacity: 0, y: 100, x:-100 }} className="absolute -left-32 top-8 hidden lg:block">
-                    <Image src={designExampleImage} alt="Design image 1" />
+                <motion.div
+                    ref={leftDesignScope}
+                    initial={{ opacity: 0, y: 100, x: -100 }}
+                    drag
+                    className="absolute -left-32 top-8 hidden lg:block"
+                >
+                    <Image
+                        src={designExampleImage}
+                        alt="Design image 1"
+                        draggable="false"
+                    />
                 </motion.div>
-                <motion.div ref={leftPointerScope} initial={{ opacity: 0, y:100, x:-200 }} className="absolute left-56 top-96 hidden lg:block">
+                <motion.div
+                    ref={leftPointerScope}
+                    initial={{ opacity: 0, y: 100, x: -200 }}
+                    className="absolute left-56 top-96 hidden lg:block"
+                >
                     <Pointer name="Andrea" />
                 </motion.div>
-                <motion.div ref={rightDesignScope} className="absolute -right-64 -top-16 hidden lg:block">
-                    <Image src={designExample2Image} alt="Design image 2" />
+                <motion.div
+                    ref={rightDesignScope}
+                    initial={{ opacity: 0, x: 100, y: 100 }}
+                    className="absolute -right-64 -top-16 hidden lg:block"
+                    drag
+                >
+                    <Image
+                        src={designExample2Image}
+                        alt="Design image 2"
+                        draggable="false"
+                    />
                 </motion.div>
-                <motion.div ref={rightPointerScope} className="absolute right-80 -top-4 hidden lg:block">
-                     <Pointer name="Bryan" color="red-gradient" />
+                <motion.div
+                    ref={rightPointerScope}
+                    initial={{ opacity: 0, x: 275, y: 100 }}
+                    className="absolute right-80 -top-4 hidden lg:block"
+                >
+                    <Pointer name="Bryan" color="red-gradient" />
                 </motion.div>
 
                 <div className="flex justify-center">
