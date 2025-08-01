32c064217e32b7a95fa61948fc8ad9cbf0d6812b
ArthurFialho <afialho305@gmail.com>
2025-08-01 13:48:55 -0300

    feat: adding Faqs q&a animation

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Tag from "@/components/Tag";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const faqs = [
@@ -25,7 +28,7 @@ const faqs = [
 ];
 
 export default function Faqs() {
-    const selectedIndex = 0;
+    const [selectedIndex, setSelectedIndex] = useState(0);
     return (
         <section className="py-24">
             <div className="container">
@@ -33,7 +36,7 @@ export default function Faqs() {
                     <Tag>Faqs</Tag>
                 </div>
                 <h2 className="text-6xl font-medium mt-6 text-center max-w-xl mx-auto">
-                    Questions? We’ve got{" "}
+                    Questions? We&apos;ve got{" "}
                     <span className="text-lime-400">answers</span>
                 </h2>
                 <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
@@ -42,7 +45,10 @@ export default function Faqs() {
                             key={faq.question}
                             className="bg-neutral-900 rounded-2xl border border-white/10 p-6"
                         >
-                            <div className="flex justify-between items-center">
+                            <div
+                                className="flex justify-between items-center"
+                                onClick={() => setSelectedIndex(faqIndex)}
+                            >
                                 <h3 className="font-medium">{faq.question}</h3>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -55,23 +61,38 @@ export default function Faqs() {
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
                                     className={twMerge(
-                                        "feather feather-plus text-lime-400 flex-shrink-0",
+                                        "feather feather-plus text-lime-400 hover:text-lime-500 flex-shrink-0 transition duration-500 hover:scale-110",
                                         selectedIndex === faqIndex &&
-                                            "rotate-45"
+                                            "rotate-90"
                                     )}
                                 >
                                     <line x1="12" y1="5" x2="12" y2="19"></line>
                                     <line x1="5" y1="12" x2="19" y2="12"></line>
                                 </svg>
                             </div>
-                            <div
-                                className={twMerge(
-                                    "mt-6",
-                                    selectedIndex !== faqIndex && "hidden"
+                            <AnimatePresence>
+                                {selectedIndex === faqIndex && (
+                                    <motion.div
+                                        initial={{
+                                            height: 0,
+                                            marginTop: 0,
+                                        }}
+                                        animate={{
+                                            height: "auto",
+                                            marginTop: 24,
+                                        }}
+                                        exit={{
+                                            height: 0,
+                                            marginTop: 0,
+                                        }}
+                                        className={twMerge("overflow-hidden")}
+                                    >
+                                        <p className="text-white/50">
+                                            {faq.answer}
+                                        </p>
+                                    </motion.div>
                                 )}
-                            >
-                                <p className="text-white/50">{faq.answer}</p>
-                            </div>
+                            </AnimatePresence>
                         </div>
                     ))}
                 </div>
